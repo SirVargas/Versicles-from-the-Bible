db625c4f1faf72c3225bb72a2e0fd5f881eacd6b
SirVargas <juanjo2108pepe@gmail.com>
2026-01-18 12:15:58 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'biblia-v28-OFFLINE-QUEUE'; // Subimos versión
+const CACHE_NAME = 'biblia-v29-QUEUE'; // Subimos versión
 const urlsToCache = [
   '/',                
   './index.html',
