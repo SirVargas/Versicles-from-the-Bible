94219046b6a7409040a6499ea3fcf992c7183aeb
SirVargas <juanjo2108pepe@gmail.com>
2026-01-18 19:30:19 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'biblia-v30-G-MODE'; // Subimos versión
+const CACHE_NAME = 'biblia-v31-Fullscreen'; // Subimos versión
 const urlsToCache = [
   '/',                
   './index.html',
